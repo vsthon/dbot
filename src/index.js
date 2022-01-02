fb79e5df9f3b8d726802286a67e8844aa1fe2905
vsthon <tootaashraf@gmail.com>
2022-01-02 13:49:12 -0800

    dbot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
 const axios = require('axios')
+const fs = require('fs/promises')
+const { constants } = require('fs')
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
@@ -11,19 +13,102 @@ let reactions = {}
 let collector = null
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
+client.once('ready', async () => {
 	console.log('Ready!');
+    try {
+        await fs.access("./src/data.json", constants.F_OK | constants.R_OK)
+        const file = await fs.open("./src/data.json")
+        const data = await file.readFile("utf-8")
+        const objectdata = JSON.parse(data)
+        const guild = await client.guilds.fetch(objectdata.guildId)
+        const channel = await guild.channels.fetch(objectdata.channelId)
+        const newmsg = await channel.messages.fetch(objectdata.msgId)
+        reactions = objectdata.reactions
+
+        const rfilter = (_, user) => !user.bot
+        collector = newmsg.createReactionCollector({filter: rfilter, dispose: true}); 
+
+        collector.on('collect', async (reaction, user) => {
+            if (reactions[reaction.emoji.name] == undefined) {
+                return
+            }
+            if (reactions[reaction.emoji.name][0] == "None") {
+                try {
+                    const role2add = await guild.roles.fetch(reactions[reaction.emoji.name][1])
+                    const member = await guild.members.fetch(user.id)
+                    member.roles.add(role2add)
+                    await user.send("Added role")
+                    return
+                } catch (_) {
+                    return
+                }
+            }
+            const data = await axios.get(`https://verify.eryn.io/api/user/${user.id}`)
+            if (!data || !data.data || !data.data.userid) return
+            if (data.data.error) return
+            const userid = data.data.robloxId
+            if (reactions[reaction.emoji.name][0] == "Group") {
+                const groupdata = await axios.get(`https://groups.roblox.com/v2/users/${userid}/groups/roles`)
+                if (!groupdata.data.errors && groupdata.data.data && groupdata.data.data.find(v => 
+                    v.group.id >= reactions[reaction.emoji.name][2] && v.role.rank == reactions[reaction.emoji.name][3])
+                ) {
+                    try {
+                        const role2add = await guild.roles.fetch(reactions[reaction.emoji.name][1])
+                        const member = await guild.members.fetch(user.id)
+                        member.roles.add(role2add)
+                        await user.send("Added role")
+                    } catch (e) {
+                        console.log(e)
+                        return
+                    }
+                }
+            } else if (reactions[reaction.emoji.name][0] == "Gamepass") {
+                const gamepassdata = await axios.get(`https://inventory.roblox.com/v1/users/${userid}/items/GamePass/${reactions[reaction.emoji.name][2]}`)
+                if (!gamepassdata.data.errors && gamepassdata.data.data && gamepassdata.data.data.length > 0) {
+                    try {
+                        const role2add = await guild.roles.fetch(reactions[reaction.emoji.name][1])
+                        const member = await guild.members.fetch(user.id)
+                        member.roles.add(role2add)
+                        await user.send("Added role")
+                    } catch (_) {
+                        return
+                    }
+                }
+            }
+        });
+        collector.on('remove', async (reaction, user) => {
+            if (reactions[reaction.emoji.name] == undefined) {
+                return
+            }
+            try {
+                const role2remove = await guild.roles.fetch(reactions[reaction.emoji.name][1])
+                const member = await guild.members.fetch(user.id)
+                member.roles.remove(role2remove)
+                await user.send("Removed role")
+            } catch (e) {
+                console.log(e)
+                return
+            }
+        })
+        collector.on('end', collected => { 
+            console.log(`Collected ${collected.size} items`); 
+        });
+        file.close()
+    } catch (error) {
+        console.log(error)
+        return
+    }
 });
 
 client.on('messageCreate', async msg => {
-    if (msg.content === "!reactionbindthing" && !inUse) {
+    if (msg.content === "!reactionbindthing" && !inUse && msg.author.id == msg.guild.ownerId) {
         inUse = true
         if (collector != null) {
             collector.stop()
         }
         const filter = r => r.author.id === msg.author.id
         const channel = msg.channel 
-        let msgs = [] 
+        let msgs = []
 
         const createEmbed = (text, footer, title, color) => 
             new MessageEmbed() 
@@ -40,12 +125,15 @@ client.on('messageCreate', async msg => {
 
         const cleanup = async (t) => { 
             msgs.forEach(m_ => m_.delete()) 
-            await channel.send({embeds: [createEmbed(t)]}) 
             inUse = false 
             msgs = [] 
+            const m2 = await channel.send({embeds: [createEmbed(t)]}) 
+            setTimeout(() => {
+                m2.delete()
+            }, 3000);
         } 
 
-        await sendMsg("Enter text:", `To stop, say "Exit".`) 
+        await sendMsg("Enter message ID to react to:", `To stop, say "Exit".`) 
 
         try { 
             let c = await msg.channel.awaitMessages({filter, max: 1, time: 5000000, errors: ['time']}) 
@@ -54,9 +142,10 @@ client.on('messageCreate', async msg => {
                 return 
             }
 
-            const text = c.first().content 
+            const msgId = c.first().content
+            reactions = {}
             while (true) { 
-                await sendMsg(`Enter reaction #${Object.keys(reactions).length}.`, `To stop, say "Exit". To finish, say "Finish".`) 
+                await sendMsg(`Enter emoji ID for reaction #${Object.keys(reactions).length}.`, `To stop, say "Exit". To finish, say "Finish".`) 
                 c = await msg.channel.awaitMessages({filter, max: 1, time: 5000000, errors: ['time']}) 
                 let resp = c.first().content 
 
@@ -67,7 +156,7 @@ client.on('messageCreate', async msg => {
                     break 
                 }
 
-                await sendMsg(`Should this reaction check for gamepasses or group roles? Enter "Gamepasses" for gamepasses, or "Groups" for groups`, `To stop, say "Exit".`) 
+                await sendMsg(`Should this reaction check for gamepasses, group roles or neither? Enter "Gamepasses" for gamepasses, "Groups" for groups, and "None" for none`, `To stop, say "Exit".`) 
                 c = await msg.channel.awaitMessages({filter, max: 1, time: 5000000, errors: ['time']}) 
 
                 if (c.first().content == "Gamepasses") {
@@ -115,7 +204,16 @@ client.on('messageCreate', async msg => {
                         cleanup("Aborted") 
                         return 
                     } 
-                    reactions[resp] = ["Group", String(c.first().content), id, rr] 
+                    reactions[resp] = ["Group", String(c.first().content), id, Number(rr)] 
+                } else if (c.first().content == "None") {                    
+                    await sendMsg(`Enter role ID for reaction #${Object.keys(reactions).length}.`, `To stop, say "Exit".`) 
+                    c = await msg.channel.awaitMessages({filter, max: 1, time: 5000000, errors: ['time']}) 
+
+                    if (c.first().content == "Exit") { 
+                        cleanup("Aborted") 
+                        return 
+                    } 
+                    reactions[resp] = ["None", String(c.first().content)]
                 } else if (c.first().content == "Exit") {
                     cleanup("Aborted") 
                     return 
@@ -125,7 +223,7 @@ client.on('messageCreate', async msg => {
                 }
             } 
             await cleanup("Success") 
-            const newmsg = await channel.send({embeds: [createEmbed(text, "Reaction stuff", "Test", '#00FFFF')]})
+            const newmsg = await msg.channel.messages.fetch(msgId)
 
             const rfilter = (_, user) => !user.bot
             collector = newmsg.createReactionCollector({filter: rfilter, dispose: true}); 
@@ -134,13 +232,24 @@ client.on('messageCreate', async msg => {
                 if (reactions[reaction.emoji.name] == undefined) {
                     return
                 }
+                if (reactions[reaction.emoji.name][0] == "None") {
+                    try {
+                        const role2add = await msg.guild.roles.fetch(reactions[reaction.emoji.name][1])
+                        const member = await msg.guild.members.fetch(user.id)
+                        member.roles.add(role2add)
+                        await user.send("Added role")
+                        return
+                    } catch (_) {
+                        return
+                    }
+                }
                 const data = await axios.get(`https://verify.eryn.io/api/user/${user.id}`)
                 if (!data) return
                 const userid = data.data.robloxId
                 if (reactions[reaction.emoji.name][0] == "Group") {
                     const groupdata = await axios.get(`https://groups.roblox.com/v2/users/${userid}/groups/roles`)
                     if (groupdata.data.data && groupdata.data.data.find(v => 
-                        v.group.id == reactions[reaction.emoji.name][2] && v.role.rank == reactions[reaction.emoji.name][3])
+                        v.group.id >= reactions[reaction.emoji.name][2] && v.role.rank == reactions[reaction.emoji.name][3])
                     ) {
                         try {
                             const role2add = await msg.guild.roles.fetch(reactions[reaction.emoji.name][1])
@@ -166,38 +275,40 @@ client.on('messageCreate', async msg => {
                     }
                 }
             });
-            collector.on('dispose', async (reaction, user) => {
+            collector.on('remove', async (reaction, user) => {
                 if (reactions[reaction.emoji.name] == undefined) {
                     return
                 }
-                if (reactions[reaction.emoji.name][0] == "Group") {
-                    try {
-                        const role2remove = await msg.guild.roles.fetch(reactions[reaction.emoji.name][1])
-                        const member = await msg.guild.members.fetch(user.id)
-                        member.roles.remove(role2remove)
-                        await user.send("Removed role")
-                    } catch (e) {
-                        console.log(e)
-                        return
-                    }
-                } else if (reactions[reaction.emoji.name][0] == "Gamepass") {
-                    try {
-                        const role2remove = await msg.guild.roles.fetch(reactions[reaction.emoji.name][1])
-                        const member = await msg.guild.members.fetch(user.id)
-                        member.roles.remove(role2remove)
-                        await user.send("Removed role")
-                    } catch (_) {
-                        console.log(e)
-                        return
-                    }
+                try {
+                    const role2remove = await msg.guild.roles.fetch(reactions[reaction.emoji.name][1])
+                    const member = await msg.guild.members.fetch(user.id)
+                    member.roles.remove(role2remove)
+                    await user.send("Removed role")
+                } catch (e) {
+                    console.log(e)
+                    return
                 }
             })
             collector.on('end', collected => { 
                 console.log(`Collected ${collected.size} items`); 
             }); 
-            Object.keys(reactions).forEach(async k => { await newmsg.react(k) })
+            await Object.keys(reactions).forEach(async k => { 
+                const remoji = await msg.guild.emojis.cache.find(emoji => emoji.name == k)
+                if (remoji) {
+                    await newmsg.react(remoji) 
+                } else {
+                    await newmsg.react(k)
+                }
+            })
+            await fs.writeFile("./src/data.json", JSON.stringify({
+                guildId: msg.guild.id,
+                channelId: msg.channel.id,
+                msgId: msgId,
+                reactions: reactions
+            }))
         } 
-        catch { 
+        catch (e) { 
+            console.log(e)
             cleanup("Exceeded time limit") 
         }
     }
